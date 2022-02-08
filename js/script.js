4c197dd65e6858656515b915e57c812514d9af94
Roman Kuzero <rombeldan@gmail.com>
2022-02-09 00:13:10 +0300

    blank answers are no longer accepted, added rating for the viewer

--- js/script.js
+++ js/script.js
@@ -14,7 +14,24 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i ++){
     const a = prompt('What was the last movie you saw?', ''),
     b = prompt('How would you rate it?', '');
-    personalMovieDB.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b !='' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('error');
     }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('You just started, keep watching movies :)');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('You are an average viewer.');
+} else if (personalMovieDB.count >= 30){
+    console.log('You are a cinephile!');
+} else{
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
